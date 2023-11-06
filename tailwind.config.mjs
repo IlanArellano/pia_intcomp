a850cc4de5298fe104e74e3ca4867ec69c288a95
IlanArellano22 <110840319+IlanArellano22@users.noreply.github.com>
2023-11-06 16:56:31 -0600

    Changes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,9 +2,17 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      transitionProperty: {
+        width: "width",
+        "max-height": "max-heigth",
+        "min-height": "min-heigth",
+      },
+    },
     colors: {
       logo: "#f4f4f4",
+      footer: "#121212",
+      "contact-container": "#B9E0AB",
     },
   },
   plugins: [],
